8d7a293a5c1abf0d23b32334f414adff262f54a5
Sahil Batla <sahilbathla1@gmail.com>
2020-01-06 01:23:37 +0530

    Add option to Go Back or Upload image in preview

--- src/components/CameraPreview/index.js
+++ src/components/CameraPreview/index.js
@@ -3,12 +3,27 @@ import './CameraPreview.scss';
 import ExifOrientationImg from 'react-exif-orientation-img';
 
 export default class ImagePreview extends Component {
+
+  componentWillMount() {
+    if (!this.props.location.state || !this.props.location.state.src) { window.location = '/'; }
+  }
+
+  goBack = () => {
+    this.props.history.goBack();
+  };
+
+  uploadPicture = () => {
+    alert('Picture uploaded successfully, going back!');
+    this.props.history.goBack();
+  };
+
   render() {
     return (
       <div className="camera-preview">
         <ExifOrientationImg className="camera-preview__image" src={this.props.location.state.src}/>
         <footer className="camera-preview__footer">
-            <button>Back</button>
+            <div className="camera-preview__footer__button-left" onClick={this.goBack}>Abbruch</div>
+            <div className="camera-preview__footer__button-right" onClick={this.uploadPicture}>Fertig</div>
         </footer>
       </div>
     )
