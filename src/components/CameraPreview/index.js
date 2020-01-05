b0457630cdd007c9f7cfc8ddcb4546fa79050309
Sahil Batla <sahilbathla1@gmail.com>
2020-01-06 00:48:02 +0530

    Use 100% width for landscape views and 100vh for portrait

--- src/components/CameraPreview/index.js
+++ src/components/CameraPreview/index.js
@@ -6,7 +6,7 @@ export default class ImagePreview extends Component {
   render() {
     return (
       <div className="camera-preview">
-        <ExifOrientationImg src={this.props.location.state.src}/>
+        <ExifOrientationImg className="camera-preview__image" src={this.props.location.state.src}/>
       </div>
     )
   }
